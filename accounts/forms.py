5e32c2cfb4f6f519e8338a2423e9739090fff395
Kushal Neupane <mkushal10@gmail.com>
2020-07-25 13:37:49 +0545

    Image File Upload

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from .models import *
 
-
-from .models import Order
-
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['user']
 
 class OrderForm(ModelForm):
 	class Meta:
 		model = Order
 		fields = '__all__'
+
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
